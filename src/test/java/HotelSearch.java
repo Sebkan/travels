f4c6ed40eb52d0c1d0fd975d4781d8f8404fb1ab
Sebastian Kanecki <seb.kanecki@gmail.com>
2022-09-27 18:31:00 +0200

    Changed check out method - calendar utilization Added travellers Added submit button click Added Assertion - hotel names

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
@@ -26,6 +29,33 @@ public class HotelSearch {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='select2-match' and text()='Dubai']")));
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
         driver.findElement(By.name("checkin")).sendKeys("17/10/2022");
-        driver.findElement(By.name("checkout")).sendKeys("20/10/2022");
+        driver.findElement(By.name("checkout")).click();
+        WebElement arrowMonth = driver.findElement(By.xpath(("(//th)[@class='next'][4]")));
+        arrowMonth.click();
+        arrowMonth.click();
+        arrowMonth.click();
+        List<WebElement> months = driver.findElements(By.xpath("//td[@class='day ' and text()='6']"));
+        for (WebElement elements : months){
+            if (elements.isDisplayed()){
+                elements.click();
+            }
+        }
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        /*List<WebElement> hotelNames = driver.findElements(By.cssSelector("h4[class='RTL go-text-right mt0 mb4 list_title']"));
+        for (WebElement names : hotelNames){
+            System.out.println(names.getText());
+        }*/
+        List<String> hotelNames = driver.findElements(By.cssSelector("h4[class='RTL go-text-right mt0 mb4 list_title']")).stream()
+                                                                                                                         .map(e->e.getAttribute("textContent"))
+                                                                                                                         .collect(Collectors.toList());
+
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
     }
 }
