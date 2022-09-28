e53ea2220f2ff02e748caabc78f54783ad8aa17c
Sebastian Kanecki <seb.kanecki@gmail.com>
2022-09-28 08:57:41 +0200

    Setting the check in/out dates via calendar method; Setting the number of Adults and Children; Clicking the search button; Assertion of the heading "No Results Found".

---
+++ src/test/java/NoResultsFound.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NoResultsFound {
+
+    @Test
+    public void noResults(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.cssSelector("input[name='checkin']")).click();
+        driver.findElement(By.xpath("(//th)[@class='next'][1]")).click();
+        driver.findElements(By.xpath("//td[text()='11']")).stream()
+                                                                       .filter(WebElement::isDisplayed)
+                                                                       .findFirst()
+                                                                       .ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//td[text()='15']")).stream()
+                                                                       .filter(WebElement::isDisplayed)
+                                                                       .findFirst()
+                                                                       .ifPresent(WebElement::click);
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+        WebElement heading = driver.findElement(By.cssSelector("h2[class='text-center']"));
+        Assert.assertEquals(heading.getText(),"No Results Found");
+    }
+}
