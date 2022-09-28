726b394a486018ccdba4e8a0c8d800167a3acda7
Sebastian Kanecki <seb.kanecki@gmail.com>
2022-09-28 08:25:21 +0200

    sign up button click filling up register form creating unique email submitting the form assertion of proper heading data display

---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,49 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SignUpTest {
+
+    @Test
+    public void signUp() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        String lastName = "Kanecki";
+        int randomNumber = (int) (Math.random()*1000);
+        String email = "test"+randomNumber+"@tester.pl";
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream()
+                        .filter(WebElement::isDisplayed)
+                        .findFirst()
+                        .ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[contains(text(),' Sign Up')]")).get(1).click();
+        driver.findElement(By.name("firstname")).sendKeys("Sebastian");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("666666666");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("test123456");
+        driver.findElement(By.name("confirmpassword")).sendKeys("test123456");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver);
+        wait.ignoring(NoSuchElementException.class);
+        wait.withTimeout(Duration.ofSeconds(3));
+        wait.pollingEvery(Duration.ofMillis(300));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[@class='RTL']")));
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+        Assert.assertTrue(heading.getText().contains(lastName));
+
+    }
+}
